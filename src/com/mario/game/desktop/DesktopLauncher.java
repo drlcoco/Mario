1dd7c67075e3fe5653ac01ba0a3af9ae17eaf0c7
drlcoco <drlcoco@hotmail.com>
2021-05-24 10:40:27 +0200

    MarioBros

---
+++ src/com/mario/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mario.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mario.game.Mario;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Mario(), config);
+	}
+}
